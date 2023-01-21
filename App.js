cb61128d5ea1685e870225c16deb58676f1d6540
aguilarklyno <aguilarklyno@gmail.com>
2023-01-22 03:20:04 +0900

    done

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider as PaperProvider } from "react-native-paper";
 import HomeScreen from "./screens/HomeScreen.js";
+import BookScreen from "./screens/BookScreen.js";
+import NameScreen from "./screens/NameScreen.js";
 import TeamScreen from "./screens/TeamScreen.js";
 import MarketScreen from "./screens/MarketScreen.js";
 import SocialScreen from "./screens/SocialScreen.js";
@@ -16,6 +18,8 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={ HomeScreen } />
+          <Stack.Screen name="Book" component={ BookScreen } />
+          <Stack.Screen name="Name" component={ NameScreen } />
           <Stack.Screen name="Team" component={ TeamScreen } />
           <Stack.Screen name="Market" component={ MarketScreen } />
           <Stack.Screen name="Social" component={ SocialScreen } />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet } from "react-native"
+import { Button, Card } from "react-native-paper";
+import { DefaultTheme } from "react-native-paper";
+import { TextInput } from "react-native-paper";
+import { Text, View } from 'react-native';
 
+function HomeScreen ({navigation}) {
 
-const HomeScreen = () => {
+  const [name, setName] =useState('');
+
   return (
-    <View>
-      <Text>
-        HomeScreen
-      </Text>
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <Card style={styles.card}>
+        <Card.Title title="Nativate to 'Book' Screen" />
+        <Card.Content>
+          <Button mode="contained" onPress={() => navigation.navigate("Book")}>
+            Navigate
+          </Button>
+        </Card.Content>
+      </Card>
+      <Card style={styles.card}>
+        <Card.Title title="Navigate with Parameters" >
+          <Card.Content>
+            <TextInput mode="outlined" label="Name" value={name} onChangeText={(text) => setName(text)} style={styles.textInput} />
+              <Button mode="contained" disabled={name.length === 0} onPress={() => navigation.navigate("Name", {name})}>
+                Navigate
+              </Button>
+          </Card.Content>
+        </Card.Title>
+      </Card>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: DefaultTheme.colors.background,
+    paddingTop: 10
+  },
+  card:{
+    width: "90%",
+    marginRight: "auto",
+    marginLeft: "auto",
+  },
+  textInput: {
+    marginBottom: 10
+  }
+})
 
 export default HomeScreen
